5eab72331394beccceb832844e7396b36c6eacf4
AFFECTWAVE <fogingohan@gmail.com>
2024-06-08 11:49:31 +0300

    Добавил команду /help

--- server.py
+++ server.py
@@ -125,13 +125,11 @@ class Server:
                         self.send_msg(message['peer_id'], "Недостаточно прав")
                         break
 
-        if "/cmd" in message['text']:
+        if "/help" in message['text']:
             for i in range(len(initiator.conference)):
                 if initiator.conference[i] == message['peer_id'] - 2000000000:
-                    if (initiator.rang[i] >=1 ) and (initiator.rang[i] > experimental.rang[i]):
-                             self.send_msg(message['peer_id'],f" {initiator.nick[i]} Cписок доступных команд")
-
-
+                    if (initiator.rang[i] >=0 ) and (initiator.rang[i] > experimental.rang[i]):
+                             self.send_msg(message['peer_id'],f" {initiator.nick[i]} Важная информация: ")
 
 
 
