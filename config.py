a7db5928cf3c14c8f0a5a8e4089a1d0d5b2e6ebe
XSerpitoX <162741916+XSerpitoX@users.noreply.github.com>
2024-06-11 18:04:55 +0400

    Update config.py
    Сокрытие конфига

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-token = "vk1.a.1lub7-5FhIRCavmUTBslhWXaSD2uifvT-OZM05c2lYScMTqTbE0u3-1k_CmWRAcpOqhkyti1P0Gygf8ZPHDzmw1-zen-WeNRKz6mScmWsHLXkEGEB7bR5GFnsndiqzFzlh79VN3nU9lZMrOIbGYS1BzCOnvHPA-xOPbU8i4sfAPqIWwp6WrTXeSwcBjBCbgId1F0ARKe5AAwijKQWpqKPQ"
-host = "127.0.0.1"
-user = 'postgres'
-password = "Admimlac132-"
-db_name = "HrozniyScriptVKBot"
+
