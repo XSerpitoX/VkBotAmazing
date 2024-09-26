9c0ec21d9e6553f98942aac96b6439e7bf333802
XSerpitoX <162741916+XSerpitoX@users.noreply.github.com>
2024-09-26 20:03:59 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = "vk1.a.fPcn9EpXHdSQg50XwGTL87gXVTGf1z40sd8umHxEVO6MoYlBSUq6Lmez_qjsni9GGfebb8EwxP7piKaK5He8_HbM_mBsbs02qTGgUu45tPQOpXItzo23TAyCVyqsvZWfV2H6Iv_VcLuXjUcFqlSrJnKwPLFsIQCcNifCnJgWc1jgPUG4WGPaaTvf-TuFo77bHaPQtI3JP1Y4FdapQCOH4Q"
+token = "vk1.a.fPcn9EpXHdSQg50XwGTL87gUcFqlSrJnKwPLFsIQCcNifCnJgWc1jgPUG4WGPaaTvf-TuFo77bHaPQtI3JP1Y4FdapQCOH4Q"
 host = "127.0.0.1"
 user = 'postgres'
 password = "Admimlac132-"
