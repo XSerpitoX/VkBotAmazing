801012e10d1e1b1398515b843cc03b3a46bfda5a
XSerpitoX <162741916+XSerpitoX@users.noreply.github.com>
2024-09-26 20:04:09 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-token = "vk1.a.fPcn9EpXHdSQg50XwGTL87gUcFqlSrJnKwPLFsIQCcNifCnJgWc1jgPUG4WGPaaTvf-TuFo77bHaPQtI3JP1Y4FdapQCOH4Q"
-host = "127.0.0.1"
-user = 'postgres'
-password = "Admimlac132-"
-db_name = "HrozniyScriptVKBot"
+
